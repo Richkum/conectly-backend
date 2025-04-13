759b562c4eaa21f96612c6731588ae2f03920f41
Richkum <kumatum2@gmail.com>
2025-04-13 02:24:57 +0100

    feat: Add nodemon dependency for development ``

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,8 +2,16 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
+import { MongooseModule } from '@nestjs/mongoose';
+import { MongooseConfigService } from './config/mongoose.config';
+
 @Module({
-  imports: [],
+  imports: [
+    // Load environment variables from .env file
+    MongooseModule.forRootAsync({
+      useClass: MongooseConfigService,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
